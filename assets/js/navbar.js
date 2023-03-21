6cc0d37e31388323604855397e34e70ae7a8af42
Karmelova <kamila.weronika.marcinek@gmail.com>
2023-03-21 19:50:13 +0100

    navbar mobile view bar

--- assets/js/navbar.js
+++ assets/js/navbar.js
@@ -1,13 +1,28 @@
-document.addEventListener('DOMContentLoaded', function() {
-    window.addEventListener('scroll', changeNavbarBackground);
-    var navbar = document.getElementById("navbar");
-    var glassMenu = navbar.offsetTop;
-    
-    function changeNavbarBackground() {
-      if (window.pageYOffset >= glassMenu+10) {
-        navbar.classList.add("glass");
-      } else {
-        navbar.classList.remove("glass");
-      }
+document.addEventListener('DOMContentLoaded', function () {
+  window.addEventListener('scroll', changeNavbarBackground);
+  var navbar = document.getElementById("navbar");
+  var glassMenu = navbar.offsetTop;
+
+  function changeNavbarBackground() {
+    if (window.pageYOffset >= glassMenu + 10) {
+      navbar.classList.add("glass");
+    } else {
+      navbar.classList.remove("glass");
     }
+  }
+
+  const menuBtn = document.querySelector('.menu-icon');
+  let menuIcon = 'hamburger';
+  menuBtn.addEventListener('click', () => {
+    if (menuIcon === 'hamburger') {
+      menuBtn.classList.remove('reverse');
+      menuBtn.classList.add('x');
+      menuIcon = 'x';
+    }
+    else {
+      menuBtn.classList.remove('x');
+      menuBtn.classList.add('reverse');
+      menuIcon = 'hamburger';
+    }
+  });
 })
