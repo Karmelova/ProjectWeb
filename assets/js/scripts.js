7a29afd4270584b33ca75585457cedf549bb3efb
Karmelova <kamila.weronika.marcinek@gmail.com>
2023-03-21 23:49:10 +0100

    logo click mobile hide-menu fix

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,42 +1,44 @@
-document.addEventListener("DOMContentLoaded", function () {
-  const navbar = document.getElementById("navbar");
-  var menuBtn = document.getElementById("nav-icon");
-  var menuItems = document.querySelector(".nav-list");
-  var glassMenu = navbar.offsetTop;
+document.addEventListener('DOMContentLoaded', function () {
+  const navbar = document.getElementById('navbar')
+  var menuBtn = document.getElementById('nav-icon')
+  var menuItems = document.querySelector('.nav-list')
+  var menuBrand = document.querySelector('.brand')
+  var glassMenu = navbar.offsetTop
 
-
-
-  window.addEventListener("scroll", changeNavbarBackground);
-  menuBtn.addEventListener("click", showMobileMenu);
-  menuItems.addEventListener("click", hideMobileMenu);
-  window.addEventListener("resize", () => {
-    navbar.classList.toggle("mobile", window.innerHeight <= 768 && window.innerHeight <= 1080)
+  window.addEventListener('scroll', changeNavbarBackground)
+  menuBtn.addEventListener('click', showMobileMenu)
+  menuItems.addEventListener('click', hideMobileMenu)
+  menuBrand.addEventListener('click', hideMobileMenu)
+  window.addEventListener('resize', () => {
+    navbar.classList.toggle(
+      'mobile',
+      window.innerHeight <= 768 && window.innerHeight <= 1080
+    )
   })
 
-  function changeNavbarBackground() {
+  function changeNavbarBackground () {
     if (window.pageYOffset >= glassMenu + 10) {
-      navbar.classList.add("glass");
+      navbar.classList.add('glass')
     } else {
-      navbar.classList.remove("glass");
+      navbar.classList.remove('glass')
     }
-  };
-
-  function showMobileMenu() {
-    if (menuBtn.classList.contains("reverse")) {
-      menuBtn.classList.remove("reverse");
-      menuBtn.classList.add("x");
-      menuItems.classList.add("show");
-    }
-    else {
-      menuBtn.classList.remove("x");
-      menuBtn.classList.add("reverse");
-      menuItems.classList.remove("show");
-    }
-  };
-  function hideMobileMenu() {
-    menuBtn.classList.remove("x");
-    menuBtn.classList.add("reverse");
-    menuItems.classList.remove("show");
   }
 
+  function showMobileMenu () {
+    if (menuBtn.classList.contains('reverse')) {
+      menuBtn.classList.remove('reverse')
+      menuBtn.classList.add('x')
+      menuItems.classList.add('show')
+    } else {
+      menuBtn.classList.remove('x')
+      menuBtn.classList.add('reverse')
+      menuItems.classList.remove('show')
+    }
+  }
+
+  function hideMobileMenu () {
+    menuBtn.classList.remove('x')
+    menuBtn.classList.add('reverse')
+    menuItems.classList.remove('show')
+  }
 })
